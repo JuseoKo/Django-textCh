e7eca60326768a7f64920f9ba7891fd124949845
maria <mariat1717@daum.net>
2022-12-22 12:51:12 +0900

    fix

--- sns/views.py
+++ sns/views.py
@@ -11,14 +11,14 @@ filenames = pj_name_load()
 
 #게시글 목록
 def index(request):
-    question_list = Question.objects.order_by('-create_date')
+    question_paging = Question.objects.order_by('-create_date')
 
     page = request.GET.get('page', '1')  # 페이지 , url에 ?page=1 추가해서 보여주기
-    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    paginator = Paginator(question_paging, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page) # 객체 생성
 
     return render(request, 'sns/index.html', {'file_list': filenames,
-                                              'question_list': page_obj})
+                                              'question_paging': page_obj})
 
 #게시글 내용
 def contents(request, content_id):
