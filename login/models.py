f9c30c401626a215e8628aa9a75d0c7734d3d1c8
maria <mariat1717@daum.net>
2023-01-02 18:24:02 +0900

    add

---
+++ login/models.py
@@ -0,0 +1,7 @@
+
+
+
+
+# Create your models here.
+#python manage.py migrate
+#python manage.py makemigrations
---
+++ login/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+def logins(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])  # 사용자 인증
+        login(request, user)  # 로그인
+        return render(request, './about/about.html')
+    else:
+        return render(request, './login/login.html')
+
+def sign(request):
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(username=request.POST['userid'], password=request.POST['password1'])
+            user.save()
+            return redirect('index')
+    return render(request, './login/sign.html')
